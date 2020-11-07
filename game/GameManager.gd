cf4b8cf94eb850d8a2e216cf7de25b8e1d5fbe40
artursu@stud.ntnu.no <artursu@stud.ntnu.no>
2020-11-07 12:19:36 +0100

    Renamed Manager.tscn into Road.tscn

---
+++ game/GameManager.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+enum States {
+	
+	MainMenu,
+	RoadLevel,
+	Pause
+	
+}
+
+var game_state = States.RoadLevel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
